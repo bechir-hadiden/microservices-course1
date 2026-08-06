e32837d26b45562494a301031a3c0308238f80f2
Bechir Hadidan <bechirhadidan8@gmail.com>
2026-08-06 12:42:52 +0100

    configure-grafana-in-argocd

--- api-gateway/src/main/java/com/techie/microservices/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/techie/microservices/gateway/config/SecurityConfig.java
@@ -19,12 +19,11 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(freeResourceUrls)
-                        .permitAll()
-                        .anyRequest().authenticated())
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+        return httpSecurity
+                .csrf(csrf -> csrf.disable()) // Désactive le CSRF pour les tests
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll()) // Autorise TOUTES les requêtes sans authentification
+                // .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())) // Commentez cette ligne
                 .build();
     }
 
